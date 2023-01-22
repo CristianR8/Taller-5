58766d0cea6a00f1122bec3dad541d6ca1a120ad
CristianR8 <cristiancamiloreyrueda@gmail.com>
2023-01-22 13:50:48 -0500

    Segundo cambio

--- grilla.js
+++ grilla.js
@@ -1,56 +1,48 @@
 
-let mostrarDatos = tablaDatos() 
-var inputFiltro = document.querySelector('#filtro')
-var datos;
-var res;
-var tr = document.querySelector('#tr')
+peticionXMLHTTP(); // Genera la tabla inicial con los datos del JSON
 
-function tablaDatos(){
-    
-    const xhttp = new XMLHttpRequest();
-    
+// Configura la modificacion de la tabla al pulsar la tecla
+document.getElementById('filtro').addEventListener("keyup", (e) => {
+    let busqueda = e.target.value.toLowerCase();
+    let trs = document.querySelectorAll("tr"); // cambiar "tr" por ".persona" para no filtrar los titulos
+    for (tr of trs)
+    {
+        if (tr.innerHTML.toLowerCase().includes(busqueda)) tr.style.display = "initial";
+        else tr.style.display = "none";
+    }
+});
 
-    xhttp.open('GET', 'datos.json', true);
-
+function peticionXMLHTTP() // Realiza la peticion de los datos JSON
+{
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState === 4 && xhttp.status === 200)
+            renderizarTabla(JSON.parse(xhttp.responseText));
+    };
+    xhttp.open("GET", "datos.json", true);
     xhttp.send();
+}
 
-    xhttp.onreadystatechange = function(){
-
-        if(this.readyState == 4 && this.status == 200){
-            datos = JSON.parse(this.responseText)
-
-            res = document.querySelector('#res')
-            res.innerHTML = '';
-            
-            for(let dato of datos)
-            
-            res.innerHTML += `
-            <tr>
-                <td>${dato.id}</td>
-                <td>${dato.name}</td>
-                <td>${dato.username}</td>
-                <td>${dato.email}</td>
-                <td>${dato.address.street}</td>
-                <td>${dato.address.suite}</td>
-                <td>${dato.address.city}</td>
-                <td>${dato.phone}</td>
-                <td>${dato.website}</td>
-                <td>${dato.company.name}</td>
-                <td>${dato.company.bs}</td>
-            </tr>          
-            `           
-            }
-
-            var searchInput = document.getElementById('filtro')
-            searchInput.addEventListener('keyup', (e) => {
-                const q = e.target.value.toLowerCase();
-                tr.querySelector("#td").textContent.toLowerCase().startsWith(q)
-                ? null
-                : (tr.style.display = "none")
-                })
-                }
-        }
-        
-
-
-
+function renderizarTabla(datos) // Renderiza la tabla completa una sola vez
+{
+    let tabla = "";
+    for (let dato of datos)
+    {
+        tabla += `
+        <tr class="tr persona">
+            <td class="td">${dato.id}</td>
+            <td class="td">${dato.name}</td>
+            <td class="td">${dato.username}</td>
+            <td class="td">${dato.email}</td>
+            <td class="td">${dato.address.street}</td>
+            <td class="td">${dato.address.suite}</td>
+            <td class="td">${dato.address.city}</td>
+            <td class="td">${dato.phone}</td>
+            <td class="td">${dato.website}</td>
+            <td class="td">${dato.company.name}</td>
+            <td class="td">${dato.company.bs}</td>
+        </tr>`;
+    }
+    document.getElementById("res").innerHTML = tabla;
+    console.log(document.getElementById("res").innerHTML);
+}
